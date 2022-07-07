150bf2cc925b3d02c5049a3fd686b391413428d3
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-07-07 09:46:03 +0530

    Update 08. Balanced Checking.cpp

--- 08. Balanced Checking.cpp
+++ 08. Balanced Checking.cpp
@@ -1,4 +1,51 @@
 
+Approach: 1
+    
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isbalanced(string s)
+{
+    stack<char>st;
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+            st.push(s[i]);
+
+        else 
+        {
+            if(s[i]==')' and st.top()=='(' || 
+               s[i]=='}' and st.top()=='{' || 
+               s[i]==']' and st.top()=='[') st.pop();
+            
+            else
+              return false;
+        }
+
+    }
+
+    if(!st.empty())
+        return false;
+    
+    return true;
+}
+
+int main()
+{
+    string s="{[()]}";
+    if(isbalanced(s))
+     cout<<"True its balanced";
+    else
+     cout<<"False, not balanced";
+}
+
+
+
+
+
+
+
+Approach: 2
 
 #include<bits/stdc++.h>
 using namespace std;
