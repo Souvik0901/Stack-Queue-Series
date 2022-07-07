f0bd979b2f0e0b8af3ff4515f70fa5575c8afd74
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-07-07 12:21:36 +0530

    Update 06. Stack Imple.. using Queue.cpp

--- 06. Stack Imple.. using Queue.cpp
+++ 06. Stack Imple.. using Queue.cpp
@@ -1,4 +1,6 @@
 
+SDE Sheet Solution:
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -54,7 +56,69 @@ int main()
     cout<<"Top Most Element After removeing : "<<s.Top();
     cout<<"\n Display the stack : ";
     s.display();
-    
-
-
 }
+
+
+
+
+
+         Leetcode Solution :
+
+            class MyQueue {
+
+            public:
+                stack<int>st1;
+                stack<int>st2;
+
+                MyQueue() {
+
+                }
+
+                void push(int x) {
+                    st1.push(x);
+                }
+
+                int pop() 
+                {
+                    if(st2.empty()){
+                        while(!st1.empty())
+                        {
+                           st2.push(st1.top());
+                            st1.pop();     
+                        }
+                    }
+
+                    int elm = st2.top();
+                    st2.pop();
+                    return elm;
+                }
+
+                int peek() 
+                {
+                    if(st2.empty()){
+                    while(!st1.empty())
+                    {
+                        st2.push(st1.top());
+                        st1.pop();
+                    }
+                    }
+                    return st2.top();
+                }
+
+
+                bool empty() 
+                {
+                    if(st1.empty() )return st2.empty();
+                       return false;
+                }
+
+            };
+
+            /**
+             * Your MyQueue object will be instantiated and called as such:
+             * MyQueue* obj = new MyQueue();
+             * obj->push(x);
+             * int param_2 = obj->pop();
+             * int param_3 = obj->peek();
+             * bool param_4 = obj->empty();
+             */
