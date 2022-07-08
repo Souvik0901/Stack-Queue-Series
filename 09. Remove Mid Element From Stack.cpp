614ac9419efc4e986cefe0627780a10ea9857af0
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-07-08 15:00:51 +0530

    Update 09. Remove Mid Element From Stack.cpp

--- 09. Remove Mid Element From Stack.cpp
+++ 09. Remove Mid Element From Stack.cpp
@@ -1,5 +1,6 @@
 
 
+Approach:1,  VSCode Solution :
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -42,4 +43,111 @@ int main()
     st.push(7);
     int size= st.size()-1;
     deleteMid(st,size);
+}
+
+
+Approach:2, GEG Solution:
+
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+    public:
+    //Function to delete middle element of a stack.
+    void deleteMid(stack<int>&s, int sizeOfStack)
+    {
+        // code here.. 
+        int midelm= 0;
+        if(sizeOfStack%2!=0)
+          midelm= (sizeOfStack+1)/2;
+        else
+          midelm= (sizeOfStack)/2+1;
+          
+        stack<int>st;
+        int count=1;
+        
+        while(count!=midelm)
+        {
+            int num= s.top();
+            s.pop();
+            st.push(num);
+            count++;
+        }
+        if(count==midelm)
+          s.pop();
+          
+        while(!st.empty())
+        {
+            s.push(st.top());
+            st.pop();
+        }
+          
+           
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int sizeOfStack;
+        cin>>sizeOfStack;
+        
+        stack<int> myStack;
+        
+        for(int i=0;i<sizeOfStack;i++)
+        {
+            int x;
+            cin>>x;
+            myStack.push(x);    
+        }
+
+            Solution ob;
+            ob.deleteMid(myStack,myStack.size());
+            while(!myStack.empty())
+            {
+                cout<<myStack.top()<<" ";
+                myStack.pop();
+            }
+        cout<<endl;
+    }   
+    return 0;
+}
+  // } Driver Code Ends
+
+
+
+Approach:3, CodeStudio Solution:
+
+void solve(stack<int>&st,int count, int size)
+{
+    if(size/2==count )
+    {
+        st.pop();
+        return;
+    }
+    int num =st.top();
+    st.pop();
+    solve(st,count+1,size);
+    st.push(num);
+
+}
+
+
+void deleteMiddle(stack<int>&inputStack, int N){
+	int count =0;
+    solve(inputStack,count,N);
+    
+   // Write your code here
+   
 }
