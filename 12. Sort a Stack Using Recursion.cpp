df6936b5293c1dcbffa599486bcd453754a777b1
Souvik Sen <88578056+hr-souvik@users.noreply.github.com>
2022-07-10 10:45:21 +0530

    Update 12. Sort a Stack Using Recursion.cpp

--- 12. Sort a Stack Using Recursion.cpp
+++ 12. Sort a Stack Using Recursion.cpp
@@ -1,4 +1,115 @@
+Approach 1: CodeStudio Solution
 
+void SortedInsert(stack<int>&st,int elm)
+{
+    if(st.empty() || st.top()<elm)
+    {
+        st.push(elm);
+        return;
+    }
+    int num = st.top();
+    st.pop();
+    SortedInsert(st,elm);
+    st.push(num);
+}
+
+void sortStack(stack<int> &stack)
+{
+	if(stack.empty())
+    {
+        return;
+    }
+    int n= stack.top();
+    stack.pop();
+    sortStack(stack);
+    SortedInsert(stack,n);
+}
+
+------------------------------------------------------------------------------------------------------------------------------------
+Approach 2: GFG Solution
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+}
+}// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+void InsertSort(int elm,stack<int>&s)
+{
+    if(s.empty()|| s.top()<elm)
+    {
+        s.push(elm);
+        return;
+    }
+    int num =s.top();
+    s.pop();
+    InsertSort(elm,s);
+    s.push(num);
+    
+}
+
+
+void SortedStack :: sort()
+{
+   //Your code here
+   if(s.empty())
+   {
+       return;
+   }
+   int n= s.top();
+   s.pop();
+   sort();
+   InsertSort(n,s);
+}
+
+
+------------------------------------------------------------------------------------------------------------------------------------
+Approach 3: VisualCode Solution
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -49,3 +160,4 @@ int main()
 
     for(int i=0; i<ans.size();i++)cout<<ans[i]<<" ";
 }
+------------------------------------------------------------------------------------------------------------------------------------------------
